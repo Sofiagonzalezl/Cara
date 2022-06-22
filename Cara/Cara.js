6e9a888de74fd5b17235bd9556f319b579be024e
Sofiagonzalezl <92395715+Sofiagonzalezl@users.noreply.github.com>
2022-06-21 19:12:49 -0500

    Update Cara.js

--- Cara/Cara.js
+++ Cara/Cara.js
@@ -1,22 +1,63 @@
-let canvas = document.querySelector("#myCanvas");
-let ctx = canvas.getcontext ("2d");
-class personaje {
-    constructor(Jx,y,angle,r){
-        this.x = x;
-        this.y = y;
-        this.angle = angle;
-        this.r = r;
+let canvas =document.querySelector("#myCanvas");
+let ctx= canvas.getContext("2d")
+class Personaje{
+    constructor(x,y,angle,r){
+        this.x=x
+        this.y=y
+        this.angle=angle
+        this.r=r
     }
-    dibujarse ( ){
-    ctx.translate (this.x), this.y;
-    ctx.arc(0,0,this.r, 0.25 * math.PI, 0.75 * Math.PI)
-    ctx.baginPath();
-    ctx.stroke();
-    ctx.rotate(-this.angle)
-    ctx.translate(-this.x,-this.y);
-let happy = new personaje(200,100,0,50);
-happy.dibujarse();
-let Santi = new personaje(200,250,0,30);
-Santi.dibujarse();
+    dibujarse(){
+ctx.translate(this.x,this.y);
+ctx.rotate(this.angle*Math.PI)
+ctx.beginPath();
+ctx.arc(0,0,this.r,0,2*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(0,0,this.r/2,0.25*Math.PI,0.75*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(-15,-11,this.r/6,0.25*Math.PI,2.25*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(15,-11,this.r/6,0.25*Math.PI,2.25*Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(-15,-11,this.r/15,0.25*Math.PI,2.25*Math.PI);
+ctx.stroke();
+ctx.fill();
+ctx.beginPath();
+ctx.arc(15,-11,this.r/15,0.25*Math.PI,2.25*Math.PI);
+ctx.stroke();
+ctx.fill();
+ctx.strokeStyle="black"
+ctx.beginPath()
+ctx.moveTo(-6,0)
+ctx.lineTo(7,6)
+ctx.lineTo(-6,10)
+ctx.stroke()
+ctx.strokeStyle="black"
+ctx.beginPath()
+ctx.moveTo(-18,0)
+ctx.lineTo(-18,1)
+ctx.stroke()
+ctx.strokeStyle="black"
+ctx.beginPath()
+ctx.moveTo(18,0)
+ctx.lineTo(18,1)
+ctx.stroke()
+ctx.translate(-this.x,-this.y)
+ctx.rotate(-this.angle*Math.PI/180)
     }
 }
+let Jeoirginha = new Personaje(200,100,0,50)
+Zeus.dibujarse();
+let Pablito = new Personaje(200,300,45,30)
+Poseidon.dibujarse();
+let Pablo = new Personaje(200,100,-45,90)
+Zeus.dibujarse();
+let Josu = new Personaje(200,300,180,70)
+POseidon.dibujarse();
+
+let contenedor =  document.querySelector("#contenedor")
+contenedor.innerHTML = "Zeus, Poseidon";
